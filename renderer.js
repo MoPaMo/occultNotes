c55d6c497639e0c92c28e6949d5c1feac62f0c79
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2021-09-08 23:09:17 +0200

    use quill

--- renderer.js
+++ renderer.js
@@ -4,3 +4,10 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+var quill = new Quill('#editor', {
+  theme: 'snow',
+  readOnly:false,
+  modules: {
+    toolbar: '#toolbar'
+  },
+});
