d21800261b8c455318cbeffb2004fa245613d247
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2021-09-10 22:57:42 +0200

    add basic login

--- renderer.js
+++ renderer.js
@@ -4,6 +4,9 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+if(!localStorage.hashpwd){
+  localStorage.hashpwd=CryptoJS.SHA256("note")
+}
 const decrypt = (text, pwd) => {
   return CryptoJS.AES.decrypt(text, pwd).toString(CryptoJS.enc.Utf8);
 };
@@ -17,5 +20,20 @@ var quill = new Quill("#editor", {
     toolbar: "#toolbar",
   },
 });
+const checkPwd=()=>{
+  if(pwdInput.value.length){
+    let hash=localStorage.hashpwd;
+    let tryHash=CryptoJS.SHA256(pwdInput.value)
+    if(tryHash==hash)
+    pwdScreen.classList.add("hidden")
+  }
+}
 let pwd = "";
 let signedin = false;
+let pwdInput = document.getElementById("pwd");
+let pwdScreen = document.getElementById("login");
+pwdInput.addEventListener("keyup", (e) => {
+  if (e.key === "Enter") {
+    checkPwd();
+  }
+});
